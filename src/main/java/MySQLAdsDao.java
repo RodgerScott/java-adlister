e4b557fac3d63cd73f423cfcf19a4a3b105f17a3
RodgerScott <rodger.scott@gmail.com>
2018-04-02 14:12:29 -0500

    holding off on rest of exercise until after Tuesday's lecture

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,5 +1,28 @@
-import java.sql.DriverManager;
+import java.sql.*;
+import java.util.List;
+
+import com.mysql.cj.jdbc.Driver;
 
 public class MySQLAdsDao implements Ads {
- DriverManager.registerDriver (new Driver());
+
+ private Connection connection = null;
+
+
+
+ public static void main(String[] args) {
+  try {
+
+      // Connection Statement
+   DriverManager.registerDriver(new Driver());
+
+   Connection connection = DriverManager.getConnection(
+           "jdbc:mysql://localhost/adlister_db?serverTimezone=UTC&useSSL=false",
+           "root",
+           "codeup"
+   );
+
+  } catch (SQLException e) {
+   e.printStackTrace();
+  }
+ }
 }
