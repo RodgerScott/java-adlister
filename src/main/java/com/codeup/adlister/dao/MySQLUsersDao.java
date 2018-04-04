5b8787b19a1062f4d877f1f2695850a161b9bf4e
RodgerScott <rodger.scott@gmail.com>
2018-04-04 14:37:32 -0500

    working on #2

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,16 +1,36 @@
 package com.codeup.adlister.dao;
+import com.codeup.adlister.Config;
 import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class MySQLUsersDao implements Users{
+    private Connection connection = null;
+
+    public MySQLUsersDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
 
     @Override
     public User findByUsername(String username) {
         PreparedStatement stmt = null;
         try {
-            String sql = "SELECT id FROM users WHERE id = ("' + username + '")";
+            String userEntry = "SELECT username FROM users WHERE username LIKE '%?%'";
+            stmt = connection.prepareStatement(userEntry);
+            stmt.setString(1, searchTerm);
 
         } catch (SQLException e) {
             throw new RuntimeException("Error finding user", e);
